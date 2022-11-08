83ab8d8fbe06db44b765a08533e6b420769b94c6
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-11-07 22:00:43 -0800

    data reading function modified

--- data_preprocessing.r
+++ data_preprocessing.r
@@ -8,10 +8,10 @@ read_data<-function(URL)
   
 }
 
-URL_list <- c("https://data.chhs.ca.gov/datastore/odata3.0/294eff96-096e-4b91-bff5-8de78fda318b",
-              "https://data.chhs.ca.gov/datastore/odata3.0/2f0efc0b-9d44-4d8a-bd4d-ed4d77cb2a04",
-              "https://data.chhs.ca.gov/datastore/odata3.0/e6828e3c-bf54-4991-865b-b4cc11719b8d",
-              "https://data.chhs.ca.gov/datastore/odata3.0/075191c4-1754-47e8-96cb-575c764c84e3",
-              "https://data.chhs.ca.gov/datastore/odata3.0/238ff746-71d9-4507-95d0-2a7b5146f4f5"
+URL_list <- c("https://data.chhs.ca.gov/dataset/f243090b-4c05-4c61-b614-7cb49b86b21d/resource/294eff96-096e-4b91-bff5-8de78fda318b/download/cdph_ssi_adult_odp_2021.csv",
+              "https://data.chhs.ca.gov/dataset/f243090b-4c05-4c61-b614-7cb49b86b21d/resource/e6828e3c-bf54-4991-865b-b4cc11719b8d/download/cdph_ssi_adult_odp_2020h1.csv",
+              "https://data.chhs.ca.gov/dataset/f243090b-4c05-4c61-b614-7cb49b86b21d/resource/eb324b4d-d2d2-41e8-aa53-f1b8380b4692/download/cdph_ssi_adult_odp_2020h2.csv",
+              "https://data.chhs.ca.gov/dataset/f243090b-4c05-4c61-b614-7cb49b86b21d/resource/238ff746-71d9-4507-95d0-2a7b5146f4f5/download/cdph_ssi_adult_odp_2019.csv"
+              
               )
-SSI_adult_patients<- mapply(read_table, URL_list, SIMPLIFY = FALSE)
+SSI_adult_patients<- lapply(URL_list,read_data)
