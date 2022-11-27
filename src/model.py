7d9e1dc6ede2ab72e28b9349faa77edb6f4518ac
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-11-27 11:18:40 -0800

    RF classifier model with model accuracy score

--- src/model.py
+++ src/model.py
@@ -1,19 +1,25 @@
 import os
+import sys
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, roc_auc_score, roc_curve, f1_score
+
 
 
 
 def import_data():
     dirname = os.path.dirname(__file__)
-    data_path = os.path.join(dirname, "./data/SSI_data_preprocessed.csv")
+    data_path = os.path.join(dirname, "../data/SSI_data_preprocessed.csv")
     data = pd.read_csv(
         data_path )
     return data
+
+
 def test_train(dataset,predictors,response):
     # RF model
     # defining features dataset without labels
@@ -27,9 +33,10 @@ def test_train(dataset,predictors,response):
     )
     return train_features, test_features, train_labels, test_labels
 
-def predict_model():
+
+def predict_model(dataset,predictors,response):
     seed = 50
-    train_features, test_features, train_labels, test_labels = test_train()
+    train_features, test_features, train_labels, test_labels = test_train(dataset,predictors,response)
     features_to_encode = train_features.columns[train_features.dtypes == object].tolist()
     col_trans = make_column_transformer(
         (OneHotEncoder(), features_to_encode),
@@ -46,16 +53,26 @@ def predict_model():
 
     pipe = make_pipeline(col_trans, rf_classifier)
     pipe.fit(train_features, train_labels)
-
+    y_pred = pipe.predict(test_features)
+    accuracy_score(test_labels, y_pred)
+    print(f"The accuracy of the model is {round(accuracy_score(test_labels, y_pred), 3) * 100} %")
+    train_probs = pipe.predict_proba(train_features)[:, 1]
+    probs = pipe.predict_proba(test_features)[:, 1]
+    train_predictions = pipe.predict(train_features)
+    print(f'Train ROC AUC Score: {roc_auc_score(train_labels, train_probs)}')
+    print(f'Test ROC AUC  Score: {roc_auc_score(test_labels, probs)}')
 
 
 
 def main():
         data = import_data()
-        del data["Facility_ID","Unnamed"]
-        response = "Infections_Reported"
+        del data["Facility_ID"]
+        del data["Unnamed: 0"]
+        data["response"] = [0 if x == 0 else 1 for x in data["Infections_Reported"]]
+        del data["Infections_Reported"]
+        response = "response"
         predictors = [col for col in data.columns if col != response]
-
+        predict_model(data,predictors,response)
 
 
 
